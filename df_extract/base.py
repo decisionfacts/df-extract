5bf01ce664ad1da8690a9696162a2ab154d43c0d
Syed <syed@decisionforce.io>
2023-08-07 16:04:28 +0530

    updated with docs and image

--- df_extract/base.py
+++ df_extract/base.py
@@ -1,7 +1,45 @@
 import abc
+from typing import Any
+
+from easyocr import easyocr
 
 from df_extract import Base
-from df_extract.image import ImageExtract
+from df_extract.utils import sync_to_async
+
+
+class ImageExtract:
+
+    def __init__(
+            self,
+            model_download_enabled: bool = False,
+            model_storage_dir: str = None,
+            lang_list: list[str] | None = None,
+            *args,
+            **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self._reader = easyocr.Reader(
+            lang_list or ['en'],
+            # gpu=False,
+            download_enabled=model_download_enabled,
+            model_storage_directory=model_storage_dir,
+        )
+
+    async def read(self, path: Any):
+        try:
+            return await sync_to_async(
+                self._reader.readtext,
+                path,
+                detail=0,
+                paragraph=True,
+                # decoder='wordbeamsearch',
+                # height_ths=100,
+                # width_ths=100
+                rotation_info=[90, 180, 270]
+            )
+        except Exception as ex:
+            print(ex)
+            return []
 
 
 class BaseExtract(abc.ABC, Base):
--- df_extract/image.py
+++ df_extract/image.py
@@ -1,54 +1,16 @@
 import json
-from typing import Any
 
 import aiofiles
-from easyocr import easyocr
 
-from df_extract import Base
 from df_extract.base import BaseExtract
 from df_extract.utils import sync_to_async, iter_to_aiter
-
-
-class ImageExtract:
-
-    def __init__(
-            self,
-            model_download_enabled: bool = False,
-            model_storage_dir: str = None,
-            lang_list: list[str] | None = None,
-            *args,
-            **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        self._reader = easyocr.Reader(
-            lang_list or ['en'],
-            # gpu=False,
-            download_enabled=model_download_enabled,
-            model_storage_directory=model_storage_dir,
-        )
-
-    async def read(self, path: Any):
-        try:
-            return await sync_to_async(
-                self._reader.readtext,
-                path,
-                detail=0,
-                paragraph=True,
-                # decoder='wordbeamsearch',
-                # height_ths=100,
-                # width_ths=100
-                rotation_info=[90, 180, 270]
-            )
-        except Exception as ex:
-            print(ex)
-            return []
 
 
 class ExtractImage(BaseExtract):
 
     async def extract_as_text(self):
         await self.remove_existing_output()
-        data = await self.read(path=self.file_path)
+        data = await self._image_extract.read(path=self.file_path)
         text = ''
         async for row in iter_to_aiter(data):
             text += row + "\n\n\n\n"
@@ -58,7 +20,7 @@ class ExtractImage(BaseExtract):
 
     async def extract_as_json(self):
         await self.remove_existing_json_output()
-        data = await self.read(path=self.file_path)
+        data = await self._image_extract.read(path=self.file_path)
 
         async with aiofiles.open(self._output_json, 'w') as fobj:
             await fobj.write(await sync_to_async(json.dumps, data, indent=4))
